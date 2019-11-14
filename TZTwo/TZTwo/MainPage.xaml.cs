7b4f05c834d4166eb269e13474474284e0a80232
иван Таранець <ivan.taranecz@gmail.com>
2019-11-14 02:35:37 +0200

    Добавьте файлы проекта.

---
+++ TZTwo/TZTwo/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TZTwo
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            double time = 30;
+            Device.StartTimer(TimeSpan.FromSeconds(time), () =>
+           {
+               DisplayAlert("Время", DateTime.Now.ToString("T"), "Close");
+               return true;
+           });
+        }
+
+
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+
+        }
+
+        private async void Button2_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Методы что использовались", "OnSleep OnStart OnResume", "Закрыть");
+        }
+    }
+}
---
+++ TZTwo/TZTwo/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TZTwo
+{
+
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            double time = 30;
+            Device.StartTimer(TimeSpan.FromSeconds(time), () =>
+            {
+                DisplayAlert("Время", DateTime.Now.ToString("T"), "Close");
+                return true;
+
+            });
+
+            
+        }
+
+        private async void Next_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+
+        }
+    }   }
